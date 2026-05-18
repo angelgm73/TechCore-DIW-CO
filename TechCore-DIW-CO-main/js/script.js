92db94581b0ccc25b43e82e10b258b468e53383c
angelgm73 <gonzalezangeldev@gmail.com>
2026-05-18 16:36:20 +0200

    Pagina inicial terminada

--- TechCore-DIW-CO-main/js/script.js
+++ TechCore-DIW-CO-main/js/script.js
@@ -1,48 +1,65 @@
-$(document).ready(function() {
-    function toggleMenu() {
-        if ($(window).width() >= 992) {
-            $(".main-nav ul").show();
-        } else {
-            $(".main-nav ul").hide();
-        }
+$(document).ready(function () {
+  function toggleMenu() {
+    if ($(window).width() >= 992) {
+      $(".main-nav ul").show();
+    } else {
+      $(".main-nav ul").hide();
     }
+  }
 
-    // estado inicial
+  // estado inicial
+  toggleMenu();
+
+  // burger
+  $("#burger-menu").click(function () {
+    $(".main-nav ul").slideToggle(300);
+    var isExpanded = $(this).attr("aria-expanded") === "true";
+    $(this).attr("aria-expanded", (!isExpanded).toString());
+  });
+
+  // resize fix
+  $(window).resize(function () {
     toggleMenu();
+  });
 
-    // burger
-    $("#burger-menu").click(function () {
-        $(".main-nav ul").slideToggle(300);
-    });
+  // Resaltado con borde azul al pasar por el producto
+  $(".producto").hover(
+    function () {
+      $(this).css("border", "2px solid #007bff");
+    },
+    function () {
+      $(this).css("border", "none");
+    },
+  );
 
-    // resize fix
-    $(window).resize(function () {
-        toggleMenu();
-    });
+  //  Efecto Zoom en las fotos
+  $(".producto img").hover(
+    function () {
+      $(this).css("transform", "scale(1.15)");
+    },
+    function () {
+      $(this).css("transform", "scale(1)");
+    },
+  );
 
-    // Resaltado con borde azul al pasar por el producto
-    $(".producto").hover(
-        function() { $(this).css("border", "2px solid #007bff"); }, 
-        function() { $(this).css("border", "none"); }
-    );
+  // Feedback en botones de Usuario
+  // Cambio a color azul para "Mi cuenta"
+  $(".usuario a:first-child").hover(
+    function () {
+      $(this).css("background-color", "#007bff");
+    },
+    function () {
+      $(this).css("background-color", "");
+    },
+  );
 
-    //  Efecto Zoom en las fotos
-    $(".producto img").hover(
-        function() { $(this).css("transform", "scale(1.15)"); }, 
-        function() { $(this).css("transform", "scale(1)"); }
-    );
-
-    // Feedback en botones de Usuario
-    // Cambio a color azul para "Mi cuenta"
-    $(".usuario a:first-child").hover(
-        function() { $(this).css("background-color", "#007bff"); }, 
-        function() { $(this).css("background-color", ""); }
-    );
-
-    // Cambio a color verde para "Mi cesta"
-    $(".usuario a:last-child").hover(
-        function() { $(this).css("background-color", "#007bff"); }, 
-        function() { $(this).css("background-color", ""); }
-    );
-    
+  // Cambio a color verde para "Mi cesta"
+  $(".usuario a:last-child").hover(
+    function () {
+      $(this).css("background-color", "#007bff");
+    },
+    function () {
+      $(this).css("background-color", "");
+    },
+  );
 });
